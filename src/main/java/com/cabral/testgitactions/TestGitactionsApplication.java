11819f1bdd056c599f122af322de80baab131cd2
Abdoul-hub <togolaabdoul4@gmail.com>
2020-12-24 13:46:16 +0000

    first commit

---
+++ src/main/java/com/cabral/testgitactions/TestGitactionsApplication.java
@@ -0,0 +1,13 @@
+package com.cabral.testgitactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGitactionsApplication.class, args);
+	}
+
+}
